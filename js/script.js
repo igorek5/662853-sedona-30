2c726d32ce3f6e256923ad238c2a3bdbbd4b66e3
igorek5 <igorbautin101@gmail.com>
2021-02-23 15:10:53 +0300

    localStorage для хранения количества взрослых и детей

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 const searchButton = document.querySelector(".search-button");
 const searchForm = document.querySelector(".search-form");
 
+const dateArrival = searchForm.querySelector("[name=arrival-date]");
+const dateOfDeparture = searchForm.querySelector("[name=date-of-departure]")
+const adults = searchForm.querySelector(".search-people-adults");
+const children = searchForm.querySelector(".search-people-children");
+
+let isStorageSupport = true;
+let adultsStorage = "";
+let childrenStorage = "";
+
+try {
+  adultsStorage = localStorage.getItem("adults");
+  childrenStorage = localStorage.getItem("children");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (isStorageSupport) {
+  adults.value = adultsStorage;
+  children.value = childrenStorage;
+}
+
 searchButton.addEventListener("click", function (evt) {
   evt.preventDefault();
   if (!searchForm.classList.contains("off-form") && !searchForm.classList.contains('on-form')) {
@@ -16,4 +37,16 @@ searchButton.addEventListener("click", function (evt) {
       };
     };
   };
+});
+
+searchForm.addEventListener("submit", function (evt) {
+  if (!dateArrival.value || !dateOfDeparture.value) {
+    evt.preventDefault();
+    console.log("error: не заполнено поле")
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("adults", adults.value);
+      localStorage.setItem("children", children.value);
+    }
+  }
 });
