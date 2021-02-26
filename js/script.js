d636309e0a1c2ebc91797c54cecd55f6106a3aa5
igorek5 <igorbautin101@gmail.com>
2021-02-26 13:21:55 +0300

    script.js для формы

--- js/script.js
+++ js/script.js
@@ -1,52 +1,34 @@
 const searchButton = document.querySelector(".search-button");
-const searchForm = document.querySelector(".search-form");
+const searchWrapper = document.querySelector(".search-wrapper");
+const searcForm = document.querySelector(".search-form");
 
-const dateArrival = searchForm.querySelector("[name=arrival-date]");
-const dateOfDeparture = searchForm.querySelector("[name=date-of-departure]")
-const adults = searchForm.querySelector(".search-people-adults");
-const children = searchForm.querySelector(".search-people-children");
-
-let isStorageSupport = true;
-let adultsStorage = "";
-let childrenStorage = "";
-
-try {
-  adultsStorage = localStorage.getItem("adults");
-  childrenStorage = localStorage.getItem("children");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-if (isStorageSupport) {
-  adults.value = adultsStorage;
-  children.value = childrenStorage;
-}
-
-searchButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  if (!searchForm.classList.contains("off-form") && !searchForm.classList.contains('on-form')) {
-    searchForm.classList.add("off-form");
-  } else {
-    if (searchForm.classList.contains("off-form")) {
-      searchForm.classList.remove("off-form");
-      searchForm.classList.add("on-form");
-    } else {
-      if (searchForm.classList.contains("on-form")) {
-        searchForm.classList.remove("on-form");
-        searchForm.classList.add("off-form");
-      };
-    };
-  };
-});
-
-searchForm.addEventListener("submit", function (evt) {
-  if (!dateArrival.value || !dateOfDeparture.value) {
+if (searchWrapper) {
+  searchWrapper.classList.remove("is-active");
+  searchButton.addEventListener("click", function (evt) {
     evt.preventDefault();
-    console.log("error: не заполнено поле")
-  } else {
-    if (isStorageSupport) {
-      localStorage.setItem("adults", adults.value);
-      localStorage.setItem("children", children.value);
+    searchWrapper.classList.toggle("is-active");
+  });
+}
+
+const checkInputValue = function (arr) {
+  let flag = true;
+  arr.forEach(element => {
+    if (!element.value) {
+      flag = false;
     }
+  });
+  return flag;
+}
+
+searcForm.addEventListener("submit", function (evt) {
+  const inputs = searcForm.querySelectorAll("input");
+  if (!checkInputValue(inputs)) {
+    evt.preventDefault();
+    searchWrapper.classList.add("error");
+    setTimeout(() => {
+      searchWrapper.classList.remove("error");
+    }, 600)
+  } else {
+    searchWrapper.classList.remove("error");
   }
 });
